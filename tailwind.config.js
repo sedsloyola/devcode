5b9cc08b5b5dbb7ee88ac29016b76d28cace8835
Asim <durgaprasadkoirala19@gmail.com>
2024-07-16 15:07:14 +0545

    Created 'Upcoming Events', 'Home Page' and added a div for 'annual publication'

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,26 @@ module.exports = {
   content: ["*"],
   theme: {
     screens: {
-      'emailFits': '382px', 
-      'inputFits': '450px', 
-      'textareaFits' : '1000px', 
-      'imageAlign': '800px', 
-      'imageResize': '900px' ,
-      'navExpand' : '700px'
+      'emailFits': '382px',
+      'inputFits': '450px',
+      'textareaFits': '1000px',
+      'imageAlign': '800px',
+      'imageResize': '900px',
+      'navExpand': '800px',
+      'logoFits': '630px',
+      'logoExpand': '750px',
+      'eventsCardFit': '500px',
+      'EventsRowWise': '1100px',
+      'EventsExpand': '1350px'
     },
     extend: {
       colors: {
-        'darkBg': '#3C096C', 
-        'hoverBg': '#9D4EDD', 
-        'lightBg': '#9D4EDD'
+        'darkBg': '#130f40',
+        'hoverBg': '#3e428a',
+        'lightBg': '#30336b'
+      },
+      backgroundImage: {
+        'staringImage': 'url("/assets/images/image3.png")'
       }
     },
   },
